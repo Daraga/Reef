cd48731b2f237aec8c5888ebd8db42ac637440f9
Daraga <38896466+Daraga@users.noreply.github.com>
2020-05-10 17:14:38 -0400

    added credit card page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-function multiplyBy() {
-    num1 = document.getEmlementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 * num2;
+function openCredit() {
+    document.getElementById("credit").style.width = "100%";
+}
 
+function closeCredit() {
+    document.getElementById("credit").style.width = "0%";
 }
